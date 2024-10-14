ef8db06057be0ba2b6fe44b08551f0a587507528
Eva Vašicová <miev@seznam.cz>
2024-10-14 15:58:54 +0200

    HW4 - calculator_oop

--- hw5_calculator_oop/HW5_calculator_class.mjs
+++ hw5_calculator_oop/HW5_calculator_class.mjs
@@ -1,1 +1,17 @@
-export class Calculator {}
+export class Calculator {
+    add(a , b) {
+        return a + b;
+    }
+
+    subtract(a , b) {
+        return a - b;
+    }
+
+    multiply(a , b) {
+        return a * b;
+    }
+
+    divide(a , b) {
+        return a / b;
+    }
+}
